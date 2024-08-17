9e2963d9b459489b81755b26a4018baee1371dd0
Balaji <153369000+Balaji251226@users.noreply.github.com>
2024-08-17 13:19:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// typing Animation
+
+var typed = new Typed(".typing",{
+    strings:["Web Designer","Web Developer","Graphic Designer","Gamer","Programmer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
